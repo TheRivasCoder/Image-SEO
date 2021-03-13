03bc6c209e31054be1bc4086b90751380d32bee7
Chris Rivas <chrisrivasstudio@gmail.com>
2021-03-12 20:43:44 -0800

    Initial commit

---
+++ image_SEO.py
@@ -0,0 +1,12 @@
+import os
+
+new_filename = 'example title for image'
+count = 0
+image_file = './images'
+for filename in os.listdir(image_file):
+    if '.jpg' not in filename:
+        continue
+    suffix = filename.split('.')[1]
+    os.rename(image_file + "/" + filename, new_filename + ' ' + str(count) + '.jpg')
+    count +=1
+
